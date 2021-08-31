50d27451c6d7559cfe9501fd4cf6d154843a97b2
Leonhard Esterbauer <leonhard.esterbauer@gmail.com>
2021-08-31 10:49:41 +0200

    init functions

---
+++ nuxt.config.js
@@ -0,0 +1,114 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'nuxt-i18n-test',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    '~/plugins/preview.client.js'
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/stylelint
+    '@nuxtjs/stylelint-module',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    '@nuxtjs/i18n',
+  ],
+
+  i18n: {
+    locales: [
+      {
+        code: 'en',
+        name: 'English'
+      },
+      {
+        code: 'es',
+        name: 'Español'
+      },
+      {
+        code: 'fr',
+        name: 'Français'
+      }
+    ],
+    strategy: 'prefix',
+    defaultLocale: 'en',
+    vueI18n: {
+      fallbackLocale: 'en',
+      messages: {
+        en: {
+          welcome: 'Welcome en'
+        },
+        fr: {
+          welcome: 'Bienvenue fr'
+        },
+        es: {
+          welcome: 'Bienvenido es'
+        }
+      }
+    },
+    // Netlify will do the language detection
+    detectBrowserLanguage: false,
+    seo: true,
+  },
+
+  sitemap: {
+    hostname: 'test.com',
+    trailingSlash: true,
+    i18n: true,
+  },
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+
+  router: {
+    trailingSlash: false
+  },
+
+  generate: {
+    // No trailing slashes for static hosting
+    subFolders: false,
+  }
+}
---
+++ store/index.js
@@ -0,0 +1,14 @@
+export const state = () => ({
+  posts: [],
+})
+export const mutations = {
+  STORE_POSTS(state, posts) {
+    state.posts = posts
+  },
+}
+export const actions = {
+  nuxtServerInit({ commit }) {
+    const posts = new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+    commit('STORE_POSTS', posts)
+  },
+}
