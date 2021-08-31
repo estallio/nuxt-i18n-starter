7ee142172032ebc48aa0e7199c759d847cf8b390
Leonhard Esterbauer <leonhard.esterbauer@gmail.com>
2021-08-31 11:08:04 +0200

    added lang redirect

--- functions/cookie-redirect.js
+++ functions/cookie-redirect.js
@@ -1,6 +1,23 @@
 exports.handler = async function(event, context) {
+  const cookies = parseCookies(event)
+  const lang = cookies['i18n_redirected']
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({message: "Hello World"})
-  };
+    statusCode: 302,
+    headers: {
+      Location: '/' + lang
+    }
+  }
+}
+
+function parseCookies (request) {
+  const list = {},
+    rc = request.headers.cookie
+
+  rc && rc.split(';').forEach(function(cookie) {
+    const parts = cookie.split('=')
+    list[parts.shift().trim()] = decodeURI(parts.join('='))
+  })
+
+  return list
 }
