286a17c42da1d4bcb3354cf7fd1745a261b5672b
Leonhard Esterbauer <leonhard.esterbauer@gmail.com>
2021-09-01 10:20:54 +0200

    commented language redirector script

--- language-redirector-workers/index.js
+++ language-redirector-workers/index.js
@@ -5,6 +5,16 @@ import acceptLanguageParser from 'accept-language-parser'
 // internal imports
 import config from './config'
 
+/*
+* Important to notice: this worker script is very general and only redirects to
+* relative language paths like '/de' and not to 'https://www.example.com/de'.
+* For this reason, it is crucial to only redirect single entrypoints like
+* 'https://www.example.com/'. This means the worker should only listen on this
+* single address and not 'https://www.example.com/*' as else the worker would
+* forward itself and and endless loop of redirections like this occur:
+* https://www.example.com/de/de/de/de/de/de/de....
+* */
+
 // root entrypoint of worker
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
@@ -35,7 +45,11 @@ function handleRequest(request) {
   ////////
 
   // get the favorite supported language from header - could return null
-  const lang = acceptLanguageParser.pick(config.supportedLanguages, request.headers.get('Accept-Language') || '') || config.defaultLanguage
+  const lang = acceptLanguageParser.pick(
+    config.supportedLanguages,
+    request.headers.get('Accept-Language') || '',
+    { loose: true }
+    ) || config.defaultLanguage
 
   // respond with the accept-language value
   return Response.redirect(url + '/' + lang, 302)
